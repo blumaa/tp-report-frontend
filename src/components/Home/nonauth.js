3e45a5f6ccbb777f8451eb731fa7b0fa253bcbb2
blumaa <blumaa@gmail.com>
2020-04-12 14:33:35 +0200

    j

--- src/components/Home/nonauth.js
+++ src/components/Home/nonauth.js
@@ -82,9 +82,9 @@ const NonAuthHome = () => {
       "X-Requested-With": "XMLHttpRequest",
       "mode" : 'cors'
     };
-    const uri = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&rankby=distance&keyword=supermarket&key=AIzaSyBJhyN7v8TJyfUU1HEMiQ1lTs4mXHJ1LtQ`;
+    const uri = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&rankby=distance&keyword=supermarket&key=AIzaSyBJhyN7v8TJyfUU1HEMiQ1lTs4mXHJ1LtQ`;
     // console.log("uri", uri);
-    const response = await fetch(uri);
+    const response = await fetch(uri, headers);
     const json = await response.json();
     // console.log("map data from json", json.status, json.results);
 
