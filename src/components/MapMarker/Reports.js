b002eced9537a5e8c78906bbffefa32bd533b43c
blumaa <blumaa@gmail.com>
2020-04-15 19:02:52 +0200

    google analytics

--- src/components/MapMarker/Reports.js
+++ src/components/MapMarker/Reports.js
@@ -8,6 +8,7 @@ import ListItemText from "@material-ui/core/ListItemText";
 import RemoveShoppingCartIcon from "@material-ui/icons/RemoveShoppingCart";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import Moment from 'react-moment';
+import 'moment/min/locales';
 
 
 
